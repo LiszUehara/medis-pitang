8d14616bdcc12f0d4ac187a24e8ab96a25c91b6f
LiszUehara <ueharalisandra@gmail.com>
2024-06-20 18:37:49 -0300

    feat: use date and uuid in create

--- src/context/discipline.jsx
+++ src/context/discipline.jsx
@@ -1,18 +1,26 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const DisciplineContext = createContext({
-    disciplines: [],
+  disciplines: [],
 });
 
-
 const DisciplineContextProvider = ({ children }) => {
-    const [disciplines, setDisciplines] = useState([]);
-    const addDisciplines  = (newDiscipline) => {
-        setDisciplines([...disciplines, newDiscipline]);
-    }
+  const [disciplines, setDisciplines] = useState([]);
+  const addDisciplines = (newDiscipline) => {
+    let createDate = new Date(newDiscipline.createDate)
+    createDate.setHours(createDate.getHours()+3)
+    setDisciplines([...disciplines, { ...newDiscipline, id: uuidv4(), createDate: createDate  }]);
+  };
+  const editDiscipline = (id, description) => {
+    let newDisciplines = disciplines;
+    let discipline = newDisciplines.find((discipline) => id === discipline.id);
+    discipline.description = description
+    setDisciplines(newDisciplines);
+  };
 
   return (
-    <DisciplineContext.Provider value={{ disciplines, addDisciplines }}>
+    <DisciplineContext.Provider value={{ disciplines, addDisciplines, editDiscipline }}>
       {children}
     </DisciplineContext.Provider>
   );
